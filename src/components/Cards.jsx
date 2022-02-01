1aba52063904d67067430d5cbd42efb061c1a4cb
Kevin Do <kevind0501@gmail.com>
2022-01-31 17:52:52 -0800

    organized films in a row

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,7 +5,7 @@ export default function Cards({ ghiblis }) {
   return (
     <div className="container">
       {ghiblis.map((item) => (
-        <div key={item.id} className="">
+        <div key={item.id} className="column">
           <li>{item.title}</li>
           <img src={item.image}></img>
         </div>
